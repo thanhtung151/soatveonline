e449d862c782c5285b600089d45ae7ded1bc8189
thanhtung151 <106794955+thanhtung151@users.noreply.github.com>
2023-05-11 15:55:35 +0700

    baocao

---
+++ soatveonline/Model/location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace soatveonline.Model
+{
+    public class location
+    {
+        public string ID { get; set; }
+        public string name { get; set; }
+        public string sumary { get; set; }
+        public string images { get; set; }
+    }
+}
