f757fba69dfa6e5151740e2f1afd99eac4411e68
thanhtung151 <106794955+thanhtung151@users.noreply.github.com>
2023-05-15 23:09:04 +0700

    bao cao tiep theo

---
+++ soatveonline/Model/tickets.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace soatveonline.Model
+{
+    public class tickets
+    {
+        public int Id { get; set; }
+        public string price { get; set; }
+        public string code { get; set; }
+        public decimal status { get; set; }
+        
+    }
+}
