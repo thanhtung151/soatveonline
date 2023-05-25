3c898823fd22fac2e4756ad317815fdd573f4417
thanhtung151 <106794955+thanhtung151@users.noreply.github.com>
2023-05-25 20:13:44 +0700

    bao cao

---
+++ soatveonline/data/AddDbcontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using soatveonline.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace soatveonline.data
+{
+    public class AddDbcontext : DbContext
+    {
+        public AddDbcontext( DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<news> 
+    }
+}
